978d2949d35eec6c15b7070e4ec409ff592953f3
Julien Donck <donckjulien@gmail.com>
2019-02-10 12:58:11 +0100

    all of it

---
+++ README.md
@@ -0,0 +1,18 @@
+# monadic.xyz homepage
+
+## Set up
+
+1. Clone repo
+2. In your terminal cd to the project folder
+3. Run `$ npm install`
+4. Run `$ gulp`
+
+## Build
+
+1. Run `gulp build`
+2. The 'dist' folder contains the build
+
+## Deploy
+
+1. Run `gulp deploy`
+2. Will deploy with surge and give you a url where it lives.
---
+++ gulpfile.js
@@ -0,0 +1,125 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var surge = require('gulp-surge')
+var browserSync = require('browser-sync');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
+
+// Basic Gulp task syntax
+gulp.task('hello', function() {
+  console.log('Hello Zell!');
+})
+
+// Development Tasks
+// -----------------
+
+// Start browserSync server
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  })
+})
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
+    .pipe(gulp.dest('app/css')) // Outputs it in the css folder
+    .pipe(browserSync.reload({ // Reloading with Browser Sync
+      stream: true
+    }));
+})
+
+// Watchers
+gulp.task('watch', function() {
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+})
+
+// Optimization Tasks
+// ------------------
+
+// Optimizing CSS and JavaScript
+gulp.task('useref', function() {
+
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'));
+});
+
+// Optimizing Images
+gulp.task('images', function() {
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    // Caching images that ran through imagemin
+    .pipe(cache(imagemin({
+      interlaced: true,
+    })))
+    .pipe(gulp.dest('dist/images'))
+});
+
+// Copying fonts
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'))
+})
+// Copying css
+gulp.task('css', function() {
+  return gulp.src('app/css/**/*')
+    .pipe(gulp.dest('dist/css'))
+})
+// Copying videos
+gulp.task('video', function() {
+  return gulp.src('app/video/**/*')
+    .pipe(gulp.dest('dist/video'))
+})
+// Copying js
+gulp.task('js', function() {
+  return gulp.src('app/js/**/*')
+    .pipe(gulp.dest('dist/js'))
+})
+
+// Cleaning
+gulp.task('clean', function() {
+  return del.sync('dist').then(function(cb) {
+    return cache.clearAll(cb);
+  });
+})
+
+gulp.task('clean:dist', function() {
+  return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*', '!dist/video', '!dist/video/**/*']);
+});
+
+// Build Sequences
+// ---------------
+
+gulp.task('default', function(callback) {
+  runSequence(['sass', 'browserSync'], 'watch',
+    callback
+  )
+})
+
+gulp.task('build', function(callback) {
+  runSequence(
+    'clean:dist',
+    'sass',
+    ['useref', 'images', 'fonts', 'css', 'js', 'video'],
+    callback
+  )
+})
+
+gulp.task('deploy', [], function () {
+  return surge({
+    project: './dist',         // Path to your static build directory
+    domain: 'unhealthy-park.surge.sh'  // Your domain or Surge subdomain
+  })
+})
